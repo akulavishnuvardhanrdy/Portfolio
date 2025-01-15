cdcff9b1cc02c23d52ed26f4d2c1700c5cd91d79
Vishnu <akulavishnuvardhanrdy@gmail.com>
2025-01-15 08:14:12 +0530

    Services and contact

---
+++ services.js
@@ -0,0 +1,40 @@
+const services = [
+    {
+      title: "NSS Volunteer",
+      description: "Organized events to promote social responsibility.",
+      bgImage: "./images/nss.jpg",
+    },
+    {
+      title: "PC Club Executive",
+      description: "Effectively managed teams and delegated tasks.",
+      bgImage: "./images/pcClub.png",
+    },
+    {
+      title: "Hackathon Organiser",
+      description: "Enhanced skills in planning, coordination.",
+      bgImage: "./images/hackathon.jpg",
+    },
+  ];
+  
+  const servicesContainer = document.getElementById("servicesContainer");
+  
+  services.forEach((service) => {
+    const serviceHTML = `
+        <div class="aspect-square bg-[url('${service.bgImage}')] bg-no-repeat bg-cover 
+        bg-center rounded-lg relative cursor-pointer group">
+          <div class="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 
+          -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+            <div>
+              <h2 class="font-semibold text-black">${service.title}</h2>
+              <p class="text-sm text-gray-700">${service.description}</p>
+            </div>
+            <div class="border rounded-full border-black w-9 aspect-square flex items-center
+            justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+              <img src="./images/send-icon.png" alt="" class="w-5">
+            </div>
+          </div>
+        </div>
+    `;
+    servicesContainer.insertAdjacentHTML("beforeend", serviceHTML);
+  });
+  
